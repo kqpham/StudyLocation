0c7934e36c3ffe7b9161be8b341de3fbab0184ac
VinceBackpack <vincent_likes_games@hotmail.com>
2017-03-20 13:24:07 -0400

    Controller
    Added file

---
+++ CommentsController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BtsIntegrated.Controllers
+{
+    public class CommentsController : Controller
+    {
+        Manager m = new Manager();
+        // GET: Comments
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Comments/Details/5
+        public ActionResult Details(int? id)
+        {
+            return View();
+        }
+
+        // GET: Comments/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Comments/Create
+        [HttpPost]
+        public ActionResult Create(CommentWithLocation newComment)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                if(!ModelState.IsValid)
+                {
+                    return View(newComment);
+                }
+
+                var addedComment = m.LocationAddComment(newComment);
+
+                if(addedComment == null)
+                {
+                    return View(newComment);
+                }
+                else
+                {
+                    return RedirectToAction("List of Comments", new { id = addedComment.CommentId });
+                }
+                
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Comments/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Comments/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Comments/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Comments/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
